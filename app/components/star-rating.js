7e3b15225d9cee9d1c001663b094d30b5e2eea0a
MoHax22rus <dominictylor13@gmail.com>
2017-03-15 13:58:50 +0200

    Add stars-rating component

---
+++ app/components/star-rating.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'div',
+  classNames: ['rating-panel'],
+  rating: 0,
+  maxRating: 5,
+  item: null,
+  onClick: '',
+
+  stars: Ember.computed('rating', 'maxRating', function() {
+    return this.starRange(this.maxRating, this.get('rating'));
+  }),
+
+  starRange (end, rating) {
+    let starsData = [];
+
+    for (let i = 1; i <= end; i++) {
+      starsData.push({
+        rating: i,
+        full: i <= rating
+      });
+    }
+
+    return starsData;
+  },
+
+  actions: {
+    setRating (newRating) {
+      this.sendAction('onClick', {
+        item: this.get('item'),
+        rating: newRating
+      });
+    }
+  }
+});
