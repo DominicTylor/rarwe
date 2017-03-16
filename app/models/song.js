ddb25ee37e5386563d15778c6af83afb6dad142c
MoHax22rus <dominictylor13@gmail.com>
2017-03-16 11:29:32 +0200

    Get data from API

--- app/models/song.js
+++ app/models/song.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
-export default Ember.Object.extend({
-  title: '',
-  band: null,
-  rating: 0
+export default DS.Model.extend({
+  title: DS.attr(),
+  band: DS.belongsTo('band'),
+  rating: DS.attr('number')
 });
--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,55 +1,15 @@
 import Ember from 'ember';
-import Band from '../models/band';
-import Song from '../models/song';
 
 export default Ember.Route.extend({
   model () {
-    let blackDog = Song.create({
-      title: 'Black dog',
-      band: 'Led Zeppelin',
-      rating: 3
-    });
-
-    let yellowLedbetter = Song.create({
-      title: 'Yellow Ledbetter',
-      band: 'Pearl Jam',
-      rating: 4
-    });
-
-    let pretender = Song.create({
-      title: 'The pretender',
-      band: 'Foo Fighters',
-      rating: 2
-    });
-
-    let daughter = Song.create({
-      title: 'Daughter',
-      band: 'Pearl Jam',
-      rating: 5
-    });
-
-    let ledZeppelin = Band.create({
-      name: 'Led Zeppelin',
-      songs: [blackDog]
-    });
-    let pearlJam = Band.create({
-      name: 'Pearl Jam',
-      description: 'Pearl Jam is an American rock band, formed in Seattle, Washington in 1990.',
-      songs: [yellowLedbetter, daughter]
-    });
-    let fooFighters = Band.create({
-      name: 'Foo Fighters',
-      songs: [pretender]
-    });
-
-    return [ledZeppelin, pearlJam, fooFighters];
+    return this.store.findAll('band');
   },
 
   actions: {
     didTransition () {
       document.title = 'Bands - Rock & Roll';
     },
-    createBand () {
+    /*createBand () {
       let controller = this.get('controller');
       let name = controller.get('name');
       let band = Band.create({
@@ -61,6 +21,6 @@ export default Ember.Route.extend({
       controller.set('name', '');
 
       this.transitionTo('bands.band.songs', band);
-    }
+    }*/
   }
 });
