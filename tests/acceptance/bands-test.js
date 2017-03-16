5c9eba284c88a9bd433dddd8da6daac2978cc976
MoHax22rus <dominictylor13@gmail.com>
2017-03-16 13:54:30 +0200

    Add Pretender and first acceptance test

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -1,7 +1,14 @@
 import {test} from 'qunit';
 import moduleForAcceptance from 'rarwe/tests/helpers/module-for-acceptance';
+import Pretender from 'pretender';
 
-moduleForAcceptance('Acceptance | bands');
+moduleForAcceptance('Acceptance | bands', {
+	afterEach() {
+		if (server && server.shutdown) {
+			server.shutdown();
+		}
+	}
+});
 
 test('visiting /', function(assert) {
 	visit('/');
@@ -19,6 +26,41 @@ test('visiting /bands', function(assert) {
 	});
 });
 
+let server;
+
 test('List bands', function(assert) {
+	server = new Pretender(function() {
+		this.get('/bands', function() {
+			let response = {
+				data: [
+					{
+						id: 1,
+						type: 'bands',
+						attributes: {
+							name: 'Radiohead'
+						}
+					},
+					{
+						id: 2,
+						type: 'bands',
+						attributes: {
+							name: 'Long Distance Calling'
+						}
+					}
+				]
+			};
+			return [200, {
+				'Content-Type': 'application/vnd.api+json'
+			}, JSON.stringify(response)];
+		});
+	});
+
 	visit('/bands');
+
+	andThen(function() {
+		assert.equal(currentURL(), '/bands');
+		assert.equal(find('.band-link').length, 2, 'All band links are rendered');
+		assert.equal(find('.band-link:contains("Radiohead")').length, 1, 'First band link contains the band name');
+		assert.equal(find('.band-link:contains("Long Distance Calling")').length, 1, 'The other band link contains the band name');
+	});
 });
