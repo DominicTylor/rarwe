10403a397842b7d29aef1644653e9444fd40d403
MoHax22rus <dominictylor13@gmail.com>
2017-03-16 19:08:40 +0200

    add acceptions test for sorting

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -3,6 +3,8 @@ import moduleForAcceptance from 'rarwe/tests/helpers/module-for-acceptance';
 import Pretender from 'pretender';
 import httpStubs from '../helpers/http-stubs';
 
+let server;
+
 moduleForAcceptance('Acceptance | bands', {
 	afterEach() {
 		if (server && server.shutdown) {
@@ -26,8 +28,6 @@ test('visiting /bands', assert => {
 		assertCurrentURL(assert, '/bands', 'Test /bands');
 	});
 });
-
-let server;
 
 test('List bands', assert => {
 	server = new Pretender(function() {
@@ -111,5 +111,73 @@ test('Create a new song in two steps', assert => {
 
 	andThen(() => {
 		assertElement(assert, '.songs .song:contains("Killer Cars")', 'Creates the song and displays it in the list');
+	});
+});
+
+test('Sort songs in various ways', assert => {
+	server =  new Pretender(function() {
+		httpStubs.stubBands(this, [
+			{
+				id: 1,
+				type: 'bands',
+				attributes: {
+					name: 'Them Crooked Vultures'
+				}
+			}
+		]);
+		httpStubs.stubSongs(this, 1, [
+			{
+				id: 1,
+				attributes: {
+					title: 'Elephants',
+					rating: 5
+				}
+			},
+			{
+				id: 2,
+				attributes: {
+					title: 'New Fang',
+					rating: 4
+				}
+			},
+			{
+				id: 3,
+				attributes: {
+					title: 'Mind Eraser, No Chaser',
+					rating: 4
+				}
+			},
+			{
+				id: 4,
+				attributes: {
+					title: 'Spinning in Daffodils',
+					rating: 5
+				}
+			}
+		]);
+	});
+
+	selectBand('Them Crooked Vultures');
+
+	andThen(() => {
+		assertCurrentURL(assert, '/bands/1/songs', 'Test /bands/1/songs');
+		assertTrimmedText(assert, '.song:first', 'Elephants', 'The first song is the highest ranked, first in the alphabet');
+		assertTrimmedText(assert, '.song:last', 'New Fang', 'The last song is the highest ranked, last in the alphabet');
+	});
+
+	click('button.sort-title-desc');
+
+	andThen(() => {
+		assertCurrentURL(assert, '/bands/1/songs?sort=titleDesc', 'Test /bands/1/songs?sort=titleDesc');
+		assertTrimmedText(assert, '.song:first', 'Spinning in Daffodils', 'The first song is the one that is the last in the alphabet');
+		assertTrimmedText(assert, '.song:last', 'Elephants', 'The last song is the one that is the last in the alphabet');
+	});
+
+	click('button.sort-rating-asc');
+
+	andThen(() => {
+		assertCurrentURL(assert, '/bands/1/songs?sort=ratingAsk', 'Test /bands/1/songs?sort=ratingAsk');
+		assertTrimmedText(assert, '.song:first', 'Mind Eraser, No Chaser', 'The first song is the lowest ranked, first in the alphabet');
+		assertTrimmedText(assert, '.song:last', 'Spinning in Daffodils', 'The last song is the highest ranked, last in the alphabet');
 	});
 });
