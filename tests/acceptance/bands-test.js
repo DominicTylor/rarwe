cae1cce0cc713e2ab2f61e792a0ab7b5544b9941
MoHax22rus <dominictylor13@gmail.com>
2017-03-16 15:35:17 +0200

    Create song acceptance test

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -10,27 +10,27 @@ moduleForAcceptance('Acceptance | bands', {
 	}
 });
 
-test('visiting /', function(assert) {
+test('visiting /', assert => {
 	visit('/');
 
-	andThen(function() {
+	andThen(() => {
 		assert.equal(currentURL(), '/bands');
 	});
 });
 
-test('visiting /bands', function(assert) {
+test('visiting /bands', assert => {
 	visit('/bands');
 
-	andThen(function() {
+	andThen(() => {
 		assert.equal(currentURL(), '/bands');
 	});
 });
 
 let server;
 
-test('List bands', function(assert) {
+test('List bands', assert => {
 	server = new Pretender(function() {
-		this.get('/bands', function() {
+		this.get('/bands', () => {
 			let response = {
 				data: [
 					{
@@ -57,7 +57,7 @@ test('List bands', function(assert) {
 
 	visit('/bands');
 
-	andThen(function() {
+	andThen(() => {
 		assert.equal(currentURL(), '/bands');
 		assert.equal(find('.band-link').length, 2, 'All band links are rendered');
 		assert.equal(find('.band-link:contains("Radiohead")').length, 1, 'First band link contains the band name');
@@ -65,9 +65,9 @@ test('List bands', function(assert) {
 	});
 });
 
-test('Create a new band', function(assert) {
-	server = new Pretender(function() {
-		this.get('/bands', function() {
+test('Create a new band', assert =>{
+	server = new Pretender(function()  {
+		this.get('/bands', () => {
 			let response = {
 				data: [
 					{
@@ -84,7 +84,7 @@ test('Create a new band', function(assert) {
 			}, JSON.stringify(response)];
 		});
 
-		this.post('/bands', function() {
+		this.post('/bands', () => {
 			let response = {
 				data: {
 					id: 2,
@@ -99,7 +99,7 @@ test('Create a new band', function(assert) {
 			}, JSON.stringify(response)];
 		});
 
-		this.get('/bands/2/songs', function() {
+		this.get('/bands/2/songs', () => {
 			let response = {
 				data: []
 			};
@@ -113,9 +113,79 @@ test('Create a new band', function(assert) {
 	fillIn('.new-band', 'Long Distance Calling');
 	click('.new-band-button');
 
-	andThen(function() {
+	andThen(() => {
 		assert.equal(find('.band-link').length, 2, 'All band links are rendered');
 		assert.equal(find('.band-link:last').text().trim(), 'Long Distance Calling', 'Created band appears at the end of the list');
 		assert.equal(find('.nav a.active:contains("Songs")').length, 1, 'The Songs tab is active');
+	});
+});
+
+test('Create a new song in two steps', assert => {
+	server = new Pretender(function() {
+		this.get('/bands', () => {
+			let response = {
+				data: [
+					{
+						id: 1,
+						type: 'bands',
+						attributes: {
+							name: 'Radiohead'
+						}
+					}
+				]
+			};
+			return [200, {
+				'Content-Type': 'application/vnd.api+json'
+			}, JSON.stringify(response)];
+		});
+
+		this.get('/bands/1', () => {
+			let response = {
+				data: {
+					id: 1,
+					type: 'bands',
+					attributes: {
+						name: 'Radiohead'
+					}
+				}
+			};
+			return [200, {
+				'Content-Type': 'application/vnd.api+json'
+			}, JSON.stringify(response)];
+		});
+
+		this.post('/songs', () => {
+			let response = {
+				data: {
+					id: 1,
+					type: 'songs',
+					attributes: {
+						name: 'Killer Cars'
+					}
+				}
+			};
+			return [200, {
+				'Content-Type': 'application/vnd.api+json'
+			}, JSON.stringify(response)];
+		});
+
+		this.get('/bands/1/songs', () => {
+			let response = {
+				data: []
+			};
+			return [200, {
+				'Content-Type': 'application/vnd.api+json'
+			}, JSON.stringify(response)];
+		});
+	});
+
+	visit('/');
+	click('.band-link:contains("Radiohead")');
+	click('a:contains("create one")');
+	fillIn('.new-song', 'Killer Cars');
+	triggerEvent('.new-song-form', 'submit');
+
+	andThen(() => {
+		assert.equal(find('.songs .song:contains("Killer Cars")').length, 1, 'Creates the song and displays it in the list');
 	});
 });
